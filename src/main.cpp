a06fb048fb6a12375d388a3d834a3b0b219856e1
raphy <raphael.stonehorse@gmail.com>
2025-08-28 17:21:38 +0200

    commit

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+#include <LightGBM/config.h>
+#include <LightGBM/dataset_loader.h>
+#include <LightGBM/boosting.h>
+#include <LightGBM/objective_function.h>
+#include <LightGBM/metric.h>
+#include <LightGBM/utils/common.h>
+
+#include <iostream>
+#include <random>
+#include <algorithm>
+
+int main(int argc, char **argv)
+{
+    return 0;
+}
