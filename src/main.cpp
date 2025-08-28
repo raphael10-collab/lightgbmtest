b27c59f6b821a498d1b9f2462746b450b352d33f
raphy <raphael.stonehorse@gmail.com>
2025-08-28 18:47:28 +0200

    commit

--- src/main.cpp
+++ src/main.cpp
@@ -11,5 +11,45 @@
 
 int main(int argc, char **argv)
 {
-    return 0;
+    /* create example dataset */
+    std::random_device rd;
+    std::mt19937 gen(rd());
+
+    // one random generator for every class
+    std::vector<std::normal_distribution<>> dists = {
+        std::normal_distribution<>(0, 1),
+        std::normal_distribution<>(10, 1)};
+
+    /* create raw data */
+    const int numSamples = 5000;
+    const int numFeats = 2;
+    const int numClasses = static_cast<int>(dists.size());
+
+    std::cout << "Num classes: " << numClasses << std::endl;
+
+    // labels
+    std::vector<float>   labels(numSamples);
+    for (int i=0; i < numSamples; i++)
+        labels[i] = i % numClasses;
+
+    std::vector< std::vector<double> > features(numSamples);
+    for (int i=0; i < numSamples; i++)
+    {
+        features[i].resize(numFeats);
+        for (int j=0; j < numFeats; j++)
+        {
+            const auto lbl = static_cast<int>(labels[i]);
+            features[i][j] = dists[lbl](gen);
+        }
+    }
+
+    // prepare sample data
+    std::vector< std::vector<double> > sampleData(numFeats);
+    for (int i=0; i < numSamples; i++)
+    {
+        for (int j=0; j < numFeats; j++)
+            sampleData[j].push_back(features[i][j]);
+    }
+
+    return EXIT_SUCCESS;
 }
